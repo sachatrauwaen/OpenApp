9fcc851ea3456e96aa6ae170a738673ed15806c6
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2017-10-21 15:25:30 +0200

    rename nuget packages

--- src/Satrabel.OpenApp.Domain/Properties/AssemblyInfo.cs
+++ src/Satrabel.OpenApp.Domain/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Satrabel.OpenApp.Core")]
+[assembly: AssemblyProduct("Satrabel.OpenApp.Domain")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
--- src/Satrabel.OpenApp.Web/Properties/AssemblyInfo.cs
+++ src/Satrabel.OpenApp.Web/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Satrabel.OpenApp.Web.Core")]
+[assembly: AssemblyProduct("Satrabel.OpenApp.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
