71f8046706e3692ac28ffa67815961d9a2891c59
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2018-07-12 14:14:39 +0200

    time field

---
+++ src/Satrabel.OpenApp.Web/Views/lib/vueforms/time.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    var timeComponent = {
+        name: "timeComponent",
+        template: '<el-time-picker v-model="model" ></el-time-picker>',
+        props: {
+            value: {},
+            schema: {},
+            prop: String,
+            options: {
+
+            },
+        },
+        computed: {
+            model: {
+                get: function () {
+                    return this.value
+                },
+                set: function (val) {
+                    this.$emit('input', val)
+                }
+            }
+        },
+    }
+    Vue.component('oa-time', timeComponent);
+})();
