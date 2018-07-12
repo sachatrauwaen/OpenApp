6ee46c8dc61bf99b7543a008438515f70b2a02b0
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2018-07-12 15:27:08 +0200

    fixe for time field

--- src/Satrabel.OpenApp.Web/Views/lib/vueforms/bundle.js
+++ src/Satrabel.OpenApp.Web/Views/lib/vueforms/bundle.js
@@ -349,6 +349,31 @@
     Vue.component('oa-checkbox-group', checkboxGroupComponent);
 })();
 (function () {
+    var timeComponent = {
+        name: "timeComponent",
+        template: '<el-time-picker v-model="model" ></el-time-picker>',
+        props: {
+            value: {},
+            schema: {},
+            prop: String,
+            options: {
+
+            },
+        },
+        computed: {
+            model: {
+                get: function () {
+                    return this.value
+                },
+                set: function (val) {
+                    this.$emit('input', val)
+                }
+            }
+        },
+    }
+    Vue.component('oa-time', timeComponent);
+})();
+(function () {
     var datetimeComponent = {
         name: "datetimeComponent",
         template: '<el-date-picker v-model="model" type="datetime" format="dd/MM/dd HH:mm" ></el-date-picker>',
