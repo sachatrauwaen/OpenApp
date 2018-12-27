b2a0d9cac8876ff93b91011c9dcfe620ebd433ad
Robrecht Siera <robrecht.siera@holoncom.eu>
2018-12-27 15:20:58 +0100

    add fr, nl, de, es, it translations of element to crud

--- src/Satrabel.OpenApp.Web/ClientApp/Crud/boot.js
+++ src/Satrabel.OpenApp.Web/ClientApp/Crud/boot.js
@@ -1,11 +1,34 @@
-﻿import Vue from 'vue'
+﻿import Vue from 'vue';
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/en'
+import localeEN from 'element-ui/lib/locale/lang/en';
+import localeFR from 'element-ui/lib/locale/lang/fr';
+import localeNL from 'element-ui/lib/locale/lang/nl';
+import localeDE from 'element-ui/lib/locale/lang/de';
+import localePT from 'element-ui/lib/locale/lang/pt';
+import localeES from 'element-ui/lib/locale/lang/es';
+import localeIT from 'element-ui/lib/locale/lang/it';
+
+let locale = localeEN;
+const loc = abp.localization.currentCulture.name;
+if (loc == 'fr') {
+    locale = localeFR;
+} else if (loc == 'nl') {
+    locale = localeNL;
+} else if (loc == 'de') {
+    locale = localeDE;
+} else if (loc == 'pt') {
+    locale = localePT;
+} else if (loc == 'es') {
+    locale = localeES;
+} else if (loc == 'it') {
+    locale = localeIT;
+}
+
 Vue.use(ElementUI, { locale });
 
-import VueCrud from 'vuecrud'
-import Layout from './layout.vue'
+import VueCrud from 'vuecrud';
+import Layout from './layout.vue';
 
 VueCrud.createApp('#app', Layout);
