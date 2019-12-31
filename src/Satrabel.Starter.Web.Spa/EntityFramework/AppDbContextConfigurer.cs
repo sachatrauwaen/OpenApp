2f8ba49eee6b40140615c4b99b4b0eaa3072ec14
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2019-12-31 18:03:20 +0100

    fix

--- src/Satrabel.Starter.Web.Spa/EntityFramework/AppDbContextConfigurer.cs
+++ src/Satrabel.Starter.Web.Spa/EntityFramework/AppDbContextConfigurer.cs
@@ -7,12 +7,12 @@ namespace Satrabel.Starter.EntityFramework
     {
         public static void Configure(DbContextOptionsBuilder<AppDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString, opt => opt.UseRowNumberForPaging());
+            builder.UseSqlServer(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<AppDbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection, opt => opt.UseRowNumberForPaging());
+            builder.UseSqlServer(connection);
         }
     }
 }
