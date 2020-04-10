4d52041e7473fa53836b4bd0cb7a89374a283fc9
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2020-04-10 15:05:19 +0200

    add ClientRootAddress setting for urls in emails

--- src/Satrabel.OpenApp.Domain/Configuration/AppSettingNames.cs
+++ src/Satrabel.OpenApp.Domain/Configuration/AppSettingNames.cs
@@ -3,5 +3,6 @@
     public static class AppSettingNames
     {
         public const string UiTheme = "App.UiTheme";
+        public const string ClientRootAddress = "App.ClientRootAddress";
     }
 }
--- src/Satrabel.OpenApp.Domain/Configuration/AppSettingProvider.cs
+++ src/Satrabel.OpenApp.Domain/Configuration/AppSettingProvider.cs
@@ -14,8 +14,9 @@ namespace Satrabel.OpenApp.Configuration
         {
             return new[]
             {
+                new SettingDefinition(AppSettingNames.ClientRootAddress, "", scopes: SettingScopes.Application , isVisibleToClients: true),
                 new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, isVisibleToClients: true),
-                new SettingDefinition(AbpZeroSettingNames.UserManagement.IsEmailConfirmationRequiredForLogin, "false", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User)
+                new SettingDefinition(AbpZeroSettingNames.UserManagement.IsEmailConfirmationRequiredForLogin, "false", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User),
             };
         }
     }
