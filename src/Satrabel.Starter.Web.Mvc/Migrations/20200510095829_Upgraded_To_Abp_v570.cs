3a8b4057ff163ca154068204efe7953a01699205
holoncom <robrecht.siera@holoncom.eu>
2020-05-10 11:59:36 +0200

    update Db Migrations

---
+++ src/Satrabel.Starter.Web.Mvc/Migrations/20200510095829_Upgraded_To_Abp_v570.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Satrabel.OpenApp.Migrations
+{
+    public partial class Upgraded_To_Abp_v570 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserNameOrEmailAddress",
+                table: "AbpUserLoginAttempts",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "AbpSettings",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AbpDynamicParameters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParameterName = table.Column<string>(nullable: true),
+                    InputType = table.Column<string>(nullable: true),
+                    Permission = table.Column<string>(nullable: true),
+                    TenantId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpDynamicParameters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpWebhookEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    WebhookName = table.Column<string>(nullable: false),
+                    Data = table.Column<string>(nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletionTime = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpWebhookEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpWebhookSubscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    TenantId = table.Column<int>(nullable: true),
+                    WebhookUri = table.Column<string>(nullable: false),
+                    Secret = table.Column<string>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    Webhooks = table.Column<string>(nullable: true),
+                    Headers = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpWebhookSubscriptions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpDynamicParameterValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true),
+                    DynamicParameterId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpDynamicParameterValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpDynamicParameterValues_AbpDynamicParameters_DynamicParameterId",
+                        column: x => x.DynamicParameterId,
+                        principalTable: "AbpDynamicParameters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpEntityDynamicParameters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntityFullName = table.Column<string>(nullable: true),
+                    DynamicParameterId = table.Column<int>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpEntityDynamicParameters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpEntityDynamicParameters_AbpDynamicParameters_DynamicParameterId",
+                        column: x => x.DynamicParameterId,
+                        principalTable: "AbpDynamicParameters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpWebhookSendAttempts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    WebhookEventId = table.Column<Guid>(nullable: false),
+                    WebhookSubscriptionId = table.Column<Guid>(nullable: false),
+                    Response = table.Column<string>(nullable: true),
+                    ResponseStatusCode = table.Column<int>(nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    TenantId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpWebhookSendAttempts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpWebhookSendAttempts_AbpWebhookEvents_WebhookEventId",
+                        column: x => x.WebhookEventId,
+                        principalTable: "AbpWebhookEvents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpEntityDynamicParameterValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(nullable: false),
+                    EntityId = table.Column<string>(nullable: true),
+                    EntityDynamicParameterId = table.Column<int>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpEntityDynamicParameterValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpEntityDynamicParameterValues_AbpEntityDynamicParameters_EntityDynamicParameterId",
+                        column: x => x.EntityDynamicParameterId,
+                        principalTable: "AbpEntityDynamicParameters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicParameters_ParameterName_TenantId",
+                table: "AbpDynamicParameters",
+                columns: new[] { "ParameterName", "TenantId" },
+                unique: true,
+                filter: "[ParameterName] IS NOT NULL AND [TenantId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicParameterValues_DynamicParameterId",
+                table: "AbpDynamicParameterValues",
+                column: "DynamicParameterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpEntityDynamicParameters_DynamicParameterId",
+                table: "AbpEntityDynamicParameters",
+                column: "DynamicParameterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpEntityDynamicParameters_EntityFullName_DynamicParameterId_TenantId",
+                table: "AbpEntityDynamicParameters",
+                columns: new[] { "EntityFullName", "DynamicParameterId", "TenantId" },
+                unique: true,
+                filter: "[EntityFullName] IS NOT NULL AND [TenantId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpEntityDynamicParameterValues_EntityDynamicParameterId",
+                table: "AbpEntityDynamicParameterValues",
+                column: "EntityDynamicParameterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpWebhookSendAttempts_WebhookEventId",
+                table: "AbpWebhookSendAttempts",
+                column: "WebhookEventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbpDynamicParameterValues");
+
+            migrationBuilder.DropTable(
+                name: "AbpEntityDynamicParameterValues");
+
+            migrationBuilder.DropTable(
+                name: "AbpWebhookSendAttempts");
+
+            migrationBuilder.DropTable(
+                name: "AbpWebhookSubscriptions");
+
+            migrationBuilder.DropTable(
+                name: "AbpEntityDynamicParameters");
+
+            migrationBuilder.DropTable(
+                name: "AbpWebhookEvents");
+
+            migrationBuilder.DropTable(
+                name: "AbpDynamicParameters");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserNameOrEmailAddress",
+                table: "AbpUserLoginAttempts",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "AbpSettings",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
