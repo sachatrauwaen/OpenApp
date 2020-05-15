26fe07c5ab89f344088faaa56b5d59ea1bf0040b
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2020-05-15 11:27:27 +0200

    add search and sorting to tenant management
    (cherry picked from commit fba6cc110a0244fc92e51e8a2b27d0d9967c874a)

---
+++ src/Satrabel.OpenApp.Application/MultiTenancy/Dto/TenantFilterDto.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Application.Services.Dto;
+using NJsonSchema.Annotations;
+
+namespace Satrabel.OpenApp.MultiTenancy.Dto
+{
+    public class TenantFilterDto : PagedAndSortedResultRequestDto
+    {
+        [JsonSchemaExtensionData("x-ui-filter-eager", true)]
+        public string Name { get; set; }
+        
+    }
+}
