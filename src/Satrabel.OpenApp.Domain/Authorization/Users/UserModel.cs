1e84446e6cb18fe7256bc89d35a4717b098ab437
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2020-11-13 10:57:51 +0100

    user and tenant registration

---
+++ src/Satrabel.OpenApp.Domain/Authorization/Users/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using Satrabel.OpenApp.Render;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Satrabel.OpenApp.Authorization.Users
+{
+    public class UserModel: EmailModel
+    {
+        public string TenantName { get; set; }
+        public string TenancyName { get; set; }
+        public string UserFullName { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ src/Satrabel.OpenApp.Domain/Render/NullViewRenderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satrabel.OpenApp.Render
+{
+    class NullViewRenderService : IViewRenderService
+    {
+        static public IViewRenderService Instance = new NullViewRenderService();
+
+        public async Task<string> RenderToStringAsync(string viewName, object model)
+        {
+            return viewName;
+        }
+    }
+}
